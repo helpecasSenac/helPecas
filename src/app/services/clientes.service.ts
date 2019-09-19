1c0f10636c94b403384583190203c37159f9290a
helpecasSenac <helpecassenac@gmail.com>
2019-09-18 21:12:01 -0300

    ATUALIZADO VICTOR SENAC 21:11H

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor(public firestore: AngularFirestore) { }
+  criar(record){
+    return this.firestore.collection('Produtos').add(record);
+   }
+  
+  listar(){
+    return this.firestore.collection('Produtos').snapshotChanges();
+   }
+  
+  alterar(recordID, record){
+    return this.firestore.doc('Produtos/' + recordID).update(record);
+   }
+  
+  excluir(recordID){
+    return this.firestore.doc('Produtos/' + recordID).delete();
+   } 
 }
+
+
+
+
+  
+  
