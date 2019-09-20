e27ff1f9288cbc8fb9746dfa98e14c6bd6f449e8
helpecasSenac <helpecassenac@gmail.com>
2019-09-20 12:01:23 -0300

    ATUALIZADO VICTOR AS 12:00H

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'produtos', loadChildren: './pages/produtos/produtos.module#ProdutosPageModule' },
   { path: 'sobre', loadChildren: './pages/sobre/sobre.module#SobrePageModule' },
   { path: 'registrar', loadChildren: './pages/registrar/registrar.module#RegistrarPageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
