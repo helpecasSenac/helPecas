a1c2a581db4bb213be3116fee43addddb42f478d
helpecasSenac <helpecasSenac@gmail.com>
2019-09-20 08:12:10 -0300

    ATUALIZADO VICTOR AS 08:11H

--- src/app/pages/produtos/produtos.page.ts
+++ src/app/pages/produtos/produtos.page.ts
@@ -1,3 +1,4 @@
+import { ProdutosService } from './../../services/produtos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produtos.page.scss'],
 })
 export class ProdutosPage implements OnInit {
+  estado: string;
+  fabricante: number;
+  modelo: string;
+  nome: string;
+  nr_serie: number;
+  preco: number;
+  
+  pro:any;
 
-  constructor() { }
+  constructor(private ProSer: ProdutosService) { }
 
   ngOnInit() {
+    this.ProSer.listar().subscribe(data => {
+      this.pro = data.map(e => {
+        return{
+          id: e.payload.doc.id,
+          isEdit: false,
+          estado: e.payload.doc.data()['estado'],
+          fabricante: e.payload.doc.data()['fabricante'],
+          modelo: e.payload.doc.data()['modelo'],
+          nome: e.payload.doc.data()['nome'],
+          nr_serie: e.payload.doc.data()['nr_serie'],
+          preco: e.payload.doc.data()['preco'],
+      
+      };
+      })
+      console.log(this.pro);
+        });
   }
-
+  /* 
+  removerRegistro(rowID) {
+    console.log(rowID);
+    this.pessoaSer.excluir(rowID);
+  }
+  
+  editarRegistro(record) {
+  
+    record.isEdit = true;
+    record.EditNome = record.nome;
+    record.EditIdade = record.idade;
+    record.EditCidade = record.cidade;
+    record.EditEmail = record.email;
+  
+  }
+  
+  atualizarRegistro(recordRow) {
+    let record = {};
+    record['nome'] = recordRow.EditNome;
+    record['idade'] = recordRow.EditIdade;
+    record['cidade'] = recordRow.EditCidade;
+    record['email'] = recordRow.EditEmail;
+    this.pessoaSer.alterar(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
+    criarRegistro(){
+  
+      let record = {};
+      record['nome'] = this.nome;
+      record['idade'] = this.idade;
+      record['cidade'] = this.cidade;
+      record['email'] = this.email;
+  
+      this.pessoaSer.criar(record).then(resp => {
+      this.nome = "";
+      this.idade = undefined;
+      this.cidade = "";
+      this.email = "";
+      console.log(resp);
+      })
+      .catch(error =>{
+        console.log(error);
+  
+      });
+    
+    }
+    */
 }
