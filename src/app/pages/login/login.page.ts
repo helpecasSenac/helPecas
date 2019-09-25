eea2ca4a00b7a8cb9aa0554a5682e828f374b1ed
helpecasSenac <helpecassenac@gmail.com>
2019-09-25 12:15:18 -0300

    ATUALIZADO VICTOR AS 12:16H

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -35,98 +35,76 @@ export class LoginPage implements OnInit {
   ngOnInit(){}
 
   segmentChanged(event: any){
-    
-   if(event.detail.value === "login") {
-     this.slides.slidePrev();
-   }
-   else{
-this.slides.slideNext();
-
-   }
+    if(event.detail.value === "login") {
+      this.slides.slidePrev();
+    }else{
+      this.slides.slideNext();
+    }
   }
 
-async login() {
-  
-  await this.presentLoading();
- 
-  try{
-    await this.authService.login(this.userLogin);
+  async login() {
+    await this.presentLoading();
+    try{
+      await this.authService.login(this.userLogin);
     }catch(error){
-this.presentToast(error.message);
+      this.presentToast(error.message);
     }finally{
       this.loading.dismiss();
     }
-
-
-}
-
- async register(){
-
-await this.presentLoading();
-
-try{
-await this.authService.register(this.userRegister);
-}catch(error){
-  let message: string;
-
-switch(error.code) {
+  }
   
-case 'auth/email-already-in-use':
-message = 'O endereço de E-mail já está registrado';
-break;
-
-case 'auth/invalid-email':
-  message = 'E-mail inválido';
-  break;
-}
-
-let record = {};
-  record['CNPJ'] = this.cnpj;
-  record['end'] = this.endereco;
-  record['nomeFant'] = this.nomefant;
-  record['razaoSoc'] = this.razaosocial;
-  record['tel'] = this.telefone;
-
-  this.authService.criar(record).then(resp => {
-    this.cnpj = "";
-    this.endereco = "";
-    this.nomefant = "";
-    this.razaosocial = "";
-    this.telefone = "";
-
-  });
- 
-
-
-  this.presentToast(message);
-
-}finally{
-  this.loading.dismiss();
-}
-
-  this.loading.dismiss();
-
-}
-
-async presentLoading() {
-  this.loading = await this.loadingCtrl.create({
+  async register(){
+    await this.presentLoading();
+    try{
+      await this.authService.register(this.userRegister);
+    }catch(error){
+      let message: string;
+      switch(error.code) {
+        case 'auth/email-already-in-use':
+          message = 'O endereço de E-mail já está registrado';
+          break;
+        case 'auth/invalid-email':
+          message = 'E-mail inválido';
+          break;
+      }
+      let record = {};
+      record['CNPJ'] = this.cnpj;
+      record['end'] = this.endereco;
+      record['nomeFant'] = this.nomefant;
+      record['razaoSoc'] = this.razaosocial;
+      record['tel'] = this.telefone;
+    
+      this.authService.criar(record).then(resp => {
+      this.cnpj = "";
+      this.endereco = "";
+      this.nomefant = "";
+      this.razaosocial = "";
+      this.telefone = "";
+      });
+  
+      this.presentToast(message);
+    }finally{
+      this.loading.dismiss();
+    }
+    this.loading.dismiss();
+  }
+  
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
     message: 'Por favor, aguarde',
-  
-  });
-  return this.loading.present();
+    });
+    return this.loading.present();
+  }
 
-}
-
-async presentToast(message: string) {
-  const toast = await this.toastCtrl.create({
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
     message,
     duration: 2000
-  });
-  toast.present();
-}
+    });
+    toast.present();
+  }
 
-ionViewWillEnter(){
-  this.menuCtrl.enable(false);
-}
-  
+  ionViewWillEnter(){
+    this.menuCtrl.enable(false);
+  }
 }
